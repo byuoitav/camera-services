8af67865209f91a424c341c82748e8b173b48fed
Daniel Randall <dgrandall@me.com>
2020-07-17 15:18:52 -0600

    fix auth

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,7 +106,7 @@ module "control_dev" {
   // required
   name           = "camera-services-control-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/control-dev"
-  image_version  = "94efcdc"
+  image_version  = "eb90525"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
