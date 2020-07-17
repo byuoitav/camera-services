7f9e70253b4350aa3de834c1d7dff2e81f0ab496
Daniel Randall <dannyrandall@users.noreply.github.com>
2020-07-17 11:51:00 -0600

    Web page (#3)
    * adding intial web project
    
    * reorg
    
    * adding in camera control webpage
    
    * cameras are now controllable
    
    * moved some stuff around
    
    * error and loading page added
    
    * working on adding auth
    
    * finish moving things
    
    * using native numpad instead of that garbo one
    
    * redirects
    
    * update go mod
    
    * more cleaning up go mod
    
    Co-authored-by: Andrew Werner <klinc9@gmail.com>

---
+++ cmd/control/web/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent{
+
+  loading = false;
+
+  constructor(private router: Router) {
+    let vh = window.innerHeight * 0.01;
+
+    // document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // window.addEventListener("resize", () => {
+    //   let vh = window.innerHeight * 0.01;
+    //   document.documentElement.style.setProperty("--vh", `${vh}px`);
+    // });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (event.url.includes("/key/")) {
+          this.loading = true;
+        }
+      }
+      if (event instanceof NavigationEnd) {
+        if (event.url.includes("/key/")) {
+          this.loading = false;
+        }
+      }
+      if (event instanceof NavigationCancel) {
+        this.loading = false;
+      }
+    });
+
+  }
+}
---
+++ cmd/control/web/src/services/room.resolver.ts
@@ -0,0 +1,47 @@
+import { Injectable, EventEmitter } from "@angular/core";
+import { Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Config } from "../objects/objects";
+import { Observable, of } from "rxjs";
+import { HttpClient, HttpRequest, HttpHeaders } from "@angular/common/http";
+import { catchError, tap, map} from 'rxjs/operators';
+import { MatDialog } from "@angular/material/dialog";
+import { ErrorDialog } from "../app/dialogs/error/error.dialog";
+
+
+@Injectable({
+    providedIn: "root"
+})
+export class RoomResolver implements Resolve<Config>{
+    constructor(private router: Router, private http: HttpClient, private dialog: MatDialog) {}
+
+    resolve(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<Config> | Observable<never> {
+        const key = route.paramMap.get("key");
+        return this.http.get<Config>("http://10.0.7.53:8080/key/" + key).pipe(       
+            catchError(err => {
+                console.log("error", err)
+                this.router.navigate([""], {})
+
+                const dialogs = this.dialog.openDialogs.filter(dialog => {
+                    return dialog.componentInstance instanceof ErrorDialog
+                })
+
+                if (dialogs.length < 1) {
+                    const ref = this.dialog.open(ErrorDialog, {
+                        width: "fit-content",
+                        data: {
+                            msg: "No cameras found for given room key"
+                        }
+                    })
+                }
+
+                return Observable.throw(err)
+            }),
+            tap(data => {
+                console.log("resolver data", data)
+            }),
+        )
+    }
+}
