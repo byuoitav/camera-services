ef35be85c12cccf896fcb1900551e60c8dd88a55
Daniel Randall <dgrandall@me.com>
2020-07-21 15:03:27 -0600

    make both aver and axis presets work

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,7 +83,7 @@ module "aver_dev" {
   // required
   name           = "camera-services-aver-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/aver-dev"
-  image_version  = "893e0a3"
+  image_version  = "0ed44e2"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
@@ -114,7 +114,7 @@ module "axis_dev" {
   // required
   name           = "camera-services-axis-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/axis-dev"
-  image_version  = "893e0a3"
+  image_version  = "0ed44e2"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
@@ -143,7 +143,7 @@ module "control_dev" {
   // required
   name           = "camera-services-control-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/control-dev"
-  image_version  = "9baaa83"
+  image_version  = "0ed44e2"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
@@ -166,7 +166,8 @@ module "control_dev" {
     "--gateway-url", data.aws_ssm_parameter.gateway_url.value,
     "--opa-url", data.aws_ssm_parameter.opa_url.value,
     "--opa-token", data.aws_ssm_parameter.control_opa_token.value,
-    "--camera-proxy", "http://camera-services-aver-dev",
+    "--aver-proxy", "http://camera-services-aver-dev",
+    "--axis-proxy", "http://camera-services-axis-dev",
   ]
   ingress_annotations = {
     // "nginx.ingress.kubernetes.io/whitelist-source-range" = "128.187.0.0/16"
