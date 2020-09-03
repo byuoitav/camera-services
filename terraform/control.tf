146755db1c376e1484aa308334a3ef8042b85772
Daniel Randall <dgrandall@me.com>
2020-09-03 16:09:52 -0600

    latest dev version

--- terraform/control.tf
+++ terraform/control.tf
@@ -28,7 +28,7 @@ module "control_dev" {
   // required
   name           = "camera-services-control-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/control-dev"
-  image_version  = "6496652"
+  image_version  = "3c852eb"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
