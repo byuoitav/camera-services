6818f5d3de39efc8cc3813844e7c7498d8e424d1
Brayden Winterton <bwinterton@gmail.com>
2020-09-07 21:50:48 -0600

    Move spyglass to using zap logger

--- cmd/spyglass/main.go
+++ cmd/spyglass/main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 	"time"
@@ -21,6 +21,7 @@ import (
 	adapter "github.com/gwatts/gin-adapter"
 	"github.com/spf13/pflag"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 const (
@@ -29,7 +30,8 @@ const (
 
 func main() {
 	var (
-		port int
+		port     int
+		logLevel string
 
 		dbAddr     string
 		dbUsername string
@@ -51,6 +53,7 @@ func main() {
 	)
 
 	pflag.CommandLine.IntVarP(&port, "port", "P", 8080, "port to run the server on")
+	pflag.StringVarP(&logLevel, "log-level", "L", "", "level to log at. refer to https://godoc.org/go.uber.org/zap/zapcore#Level for options")
 	pflag.StringVar(&dbAddr, "db-address", "", "database address")
 	pflag.StringVar(&dbUsername, "db-username", "", "database username")
 	pflag.StringVar(&dbPassword, "db-password", "", "database password")
@@ -64,6 +67,46 @@ func main() {
 	pflag.StringVar(&opaToken, "opa-token", "", "The token to use for OPA")
 	pflag.BoolVar(&disableAuth, "disable-auth", false, "Disable all auth z/n checks")
 	pflag.StringVar(&controlURLFormat, "control-url", "https://cameras.av.byu.edu/key-login?key=%s", "The url format string of the camera control service")
+
+	var level zapcore.Level
+	if err := level.Set(logLevel); err != nil {
+		fmt.Printf("invalid log level: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	config := zap.Config{
+		Level:       zap.NewAtomicLevelAt(level),
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding: "json", EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "@",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "trace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.LowercaseLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.StringDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		fmt.Printf("unable to build logger: %s", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		_ = log.Sync()
+	}()
 
 	pflag.Parse()
 
@@ -84,7 +127,7 @@ func main() {
 
 	cs, err := couch.New(ctx, dbAddr, csOpts...)
 	if err != nil {
-		log.Fatalf("unable to create config service: %s", err)
+		log.Fatal("unable to create config service", zap.Error(err))
 	}
 
 	handlers := Handlers{
@@ -133,7 +176,7 @@ func main() {
 		log.Fatal("unable to bind listener", zap.Error(err))
 	}
 
-	log.Printf("Starting server on %s", lis.Addr().String())
+	log.Info("Starting server", zap.String("on", lis.Addr().String()))
 	err = r.RunListener(lis)
 	switch {
 	case errors.Is(err, http.ErrServerClosed):
