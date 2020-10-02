0306e48b4954b33b28be64f833f43345fe81f1b8
Daniel Randall <dgrandall@me.com>
2020-10-02 10:38:18 -0600

    oh yeah. i did reboot, now i moved confirm on set preset dialog

--- terraform/control.tf
+++ terraform/control.tf
@@ -28,7 +28,7 @@ module "control_dev" {
   // required
   name           = "camera-services-control-dev"
   image          = "docker.pkg.github.com/byuoitav/camera-services/control-dev"
-  image_version  = "5259f16"
+  image_version  = "23155e5"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/camera-services"
 
