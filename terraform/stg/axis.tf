3d798724353384dac4c28deb00563f79445ae546
Clinton Reeder <clinton_reeder@byu.edu>
2024-06-04 10:32:16 -0600

    Terraform documents

---
+++ terraform/stg/axis.tf
@@ -0,0 +1,55 @@
+module "axis_dev" {
+  source = "github.com/byuoitav/terraform//modules/kubernetes-deployment"
+
+  // required
+  name           = "camera-services-axis-dev"
+  image          = "docker.pkg.github.com/byuoitav/camera-services/axis-dev"
+  image_version  = "f887685"
+  container_port = 8080
+  repo_url       = "https://github.com/byuoitav/camera-services"
+
+  // optional
+  image_pull_secret = "github-docker-registry"
+  public_urls       = ["axis-dev.av.byu.edu"]
+  private           = true
+  container_env = {
+    "GIN_MODE" = "release"
+  }
+  container_args = [
+    "--port", "8080",
+    "--log-level", "info",
+    "--name", "k8s-camera-services-axis-dev",
+    "--event-url", data.aws_ssm_parameter.event_url.value,
+    "--dns-addr", data.aws_ssm_parameter.dns_addr.value,
+  ]
+  health_check = false
+}
+
+module "axis" {
+  source = "github.com/byuoitav/terraform//modules/kubernetes-deployment"
+
+  // required
+  name           = "camera-services-axis"
+  image          = "docker.pkg.github.com/byuoitav/camera-services/axis-dev"
+  image_version  = "e4e4048"
+  container_port = 8080
+  repo_url       = "https://github.com/byuoitav/camera-services"
+
+  // optional
+  image_pull_secret = "github-docker-registry"
+  public_urls       = ["axis.av.byu.edu"]
+  replicas          = 2
+  private           = true
+  container_env = {
+    "GIN_MODE" = "release"
+  }
+  container_args = [
+    "--port", "8080",
+    "--log-level", "info",
+    "--name", "k8s-camera-services-axis",
+    "--event-url", data.aws_ssm_parameter.event_url.value,
+    "--dns-addr", data.aws_ssm_parameter.dns_addr.value,
+  ]
+  health_check = false
+}
+
---
+++ terraform/stg/spyglass.tf
@@ -0,0 +1,43 @@
+data "aws_ssm_parameter" "spyglass_opa_token" {
+  name = "/env/camera-services/spyglass/opa-token"
+}
+
+data "aws_ssm_parameter" "spyglass_client_id" {
+  name = "/env/camera-services/spyglass/client-id"
+}
+
+data "aws_ssm_parameter" "spyglass_client_secret" {
+  name = "/env/camera-services/spyglass/client-secret"
+}
+
+module "spyglass" {
+  source = "github.com/byuoitav/terraform//modules/kubernetes-deployment"
+
+  // required
+  name           = "camera-services-spyglass"
+  image          = "docker.pkg.github.com/byuoitav/camera-services/camera-spyglass-dev"
+  image_version  = "7fa936f"
+  container_port = 8080
+  repo_url       = "https://github.com/byuoitav/camera-services"
+
+  // optional
+  image_pull_secret = "github-docker-registry"
+  public_urls       = ["spyglass.av.byu.edu"]
+  container_env = {
+    "GIN_MODE" = "release"
+  }
+  container_args = [
+    "--port", "8080",
+    "--db-address", data.aws_ssm_parameter.prd_db_addr.value,
+    "--db-username", data.aws_ssm_parameter.prd_db_username.value,
+    "--db-password", data.aws_ssm_parameter.prd_db_password.value,
+    "--key-service", "control-keys",
+    "--callback-url", "https://spyglass.av.byu.edu",
+    "--client-id", data.aws_ssm_parameter.spyglass_client_id.value,
+    "--client-secret", data.aws_ssm_parameter.spyglass_client_secret.value,
+    "--gateway-url", data.aws_ssm_parameter.gateway_url.value,
+    "--opa-url", data.aws_ssm_parameter.opa_url.value,
+    "--opa-token", data.aws_ssm_parameter.spyglass_opa_token.value
+  ]
+  health_check = false
+}
